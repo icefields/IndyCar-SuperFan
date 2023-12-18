1991f9c28d02e3b70232c0024f33a32f089021cc
icefields <arctic.icefields@slmails.com>
2023-12-17 22:47:44 -0500

    added lifecycle listener

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 buildscript {
     dependencies {
-        val nav_version = "2.7.5"
+        val nav_version = "2.7.6"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
